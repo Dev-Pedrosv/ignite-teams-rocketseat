832259c933de84ad4a5ab3c214c5c61b90fad142
Pedro Henrique da Silva <pedro_rike@hotmail.com.br>
2022-11-12 18:37:41 -0300

    feat: create project ignite teams

---
+++ src/screens/groups/index.tsx
@@ -0,0 +1,10 @@
+import { Header } from "@components/header";
+import * as C from "./styles";
+
+export function Groups() {
+  return (
+    <C.Container>
+      <Header />
+    </C.Container>
+  );
+}
